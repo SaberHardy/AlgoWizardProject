809cc9c2311327d62f4631e1d803866478a063a7
SaberHardy <samirsamado5@gmail.com>
2025-01-02 22:03:07 -0700

    upload method

--- AlgoWizard/views.py
+++ AlgoWizard/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'AlgoWizard/index.html', context={})
+
+def upload(request):
+    return render(request, 'AlgoWizard/upload.html', context={})
